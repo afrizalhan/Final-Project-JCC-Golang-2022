06c7aa9d28277cf3d6838c6f761f7e8b1ca85027
afrizalhan <afrizalraehan6@gmail.com>
2022-04-03 04:58:14 +0700

    deployment prep

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "Final-Project/config"
     "Final-Project/docs"
     "Final-Project/routes"
+    "Final-Project/utils"
     "log"
     "github.com/joho/godotenv"
 )
@@ -19,15 +20,22 @@ import (
 
 func main() {
 
-    err := godotenv.Load()
-    if err != nil {
+    // for load godotenv
+    environment := utils.Getenv("ENVIRONMENT", "development")
+
+    if environment == "development" {
+      err := godotenv.Load()
+
+      if err != nil {
         log.Fatal("Error loading .env file")
+      }
     }
+
     // programmatically set swagger info
     docs.SwaggerInfo.Title = "Ecommerce API"
     docs.SwaggerInfo.Description = "This is a Ecommerce API server."
     docs.SwaggerInfo.Version = "1.0"
-    docs.SwaggerInfo.Host = "localhost:8080"
+    docs.SwaggerInfo.Host = utils.Getenv("SWAGGER_HOST", "localhost:8080")
     docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
     // database connection
